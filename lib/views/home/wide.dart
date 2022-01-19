11582581f2a2caea00b080010da4d31eef7b67f4
Blake Leonard <blake@1024256.xyz>
2022-01-18 23:07:49 -0500

    Even more quick fixes...?
    Signed-off-by: Blake Leonard <blake@1024256.xyz>

--- lib/views/home/wide.dart
+++ lib/views/home/wide.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:easy_localization/src/public_ext.dart';
+import 'package:easy_localization/easy_localization.dart';
 import "package:flutter/material.dart";
 import 'package:go_router/go_router.dart';
 import 'package:mdi/mdi.dart';
--- lib/views/settings/theming.dart
+++ lib/views/settings/theming.dart
@@ -1,4 +1,4 @@
-import 'package:easy_localization/src/public_ext.dart';
+import 'package:easy_localization/easy_localization.dart';
 import "package:flutter/material.dart";
 import "package:get/get.dart";
 import 'package:seaworld/helpers/config.dart';
